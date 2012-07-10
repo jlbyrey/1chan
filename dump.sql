81a548d8217269849f480b512581290b881dc3f4
Andrey Piontkovskiy <1kun.ebet.sobak@gmail.com>
2012-07-10 19:44:48 +0400

    Update master

--- dump.sql
+++ dump.sql
@@ -34,7 +34,7 @@ CREATE TABLE `1chan_category` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`),
   KEY `code` (`code`)
-) ENGINE=MyISAM AUTO_INCREMENT=38 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
